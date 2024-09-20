7e3412412f7bf1576b677881f700c07ffe588864
Secillla <secilafci@gmail.com>
2024-09-20 17:21:33 +0300

    fixed forecast display

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ function search(event) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
   axios.get(apiUrl).then(formatDate);
+  let apiURLtwo = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURLtwo).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -30,7 +32,7 @@ function displayTemperature(response) {
   let iconSource = response.data.condition.icon_url;
   let tempIcon = document.getElementById("temp-icon");
   tempIcon.setAttribute("src", "iconSource");
-  getForecast(response.data.city);
+  search(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -60,11 +62,7 @@ function formatDay(unformattedTime) {
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[date.getDay()];
 }
-function getForecast(city) {
-  let apiKey = "a86o1049tacf8330d5330da1fb0a51fa";
-  let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  axios(apiURL).then(displayForecast);
-}
+
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = "";
@@ -91,5 +89,4 @@ function displayForecast(response) {
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
-
 search("Paris");
